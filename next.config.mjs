6e04b6252213487dc94a17fb2dcd628ef1941f01
Ehsan <ehsannajaar@gmail.com>
2024-11-16 16:39:40 +0330

    last try to fix img of tvshow and movie problem in the project

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,23 @@ const nextConfig = {
         hostname: 'image.tmdb.org',
       },
     ],
+    unoptimized: true, // غیرفعال کردن بهینه‌سازی تصاویر
+    loader: 'imgix', // اگر نیاز دارید می‌توانید از imgix به عنوان لودر استفاده کنید
+    path: 'https://image.tmdb.org/t/p/', // مسیر پیش‌فرض برای تصاویر
+  },
+  // تنظیمات مربوط به Cache-Control
+  async headers() {
+    return [
+      {
+        source: '/_next/image', // مسیر درست
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable', // مدت زمان کش طولانی برای تصاویر
+          },
+        ],
+      },
+    ]
   },
 }
 
